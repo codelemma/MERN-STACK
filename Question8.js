6712e910d0de80ca0173f2c4fd69ed430484f389
Abhishek Shukla <106401929+codelemma@users.noreply.github.com>
2023-06-30 00:09:14 +0530

    Update Question8.js

--- Question8.js
+++ Question8.js
@@ -5,5 +5,21 @@ Only number and must be 10 digit long
 3. number should be differernt 
  */
 function validateNum(x=0){
-
+    if(typeof(x) ==='number'){
+        var a = x+"";
+        if(a.match("[+]?[0-9]{10,12}")){
+            console.log("Valid number");
+        }
+        else{
+            console.log("Please enter the correct number");
+        }
+    }
+    else{
+        console.log("Please enter in correct format");
+    }
 }
+validateNum(+919265238945);
+/*
+OUTPUT
+Valid number
+ */

f0523b9bd6083262da3abb42da75855d84bc7b98
Abhishek Shukla <106401929+codelemma@users.noreply.github.com>
2023-06-30 00:04:47 +0530

    Update question10.js

--- question10.js
+++ question10.js
@@ -1,7 +1,8 @@
 /*
  Write a JS Function to find the occurrence of each character in JS
 Input : “brain mentors”
-O/P : b 1
+O/P : 
+b 1
 r 2
 A 1
 I 1
@@ -11,4 +12,44 @@ E 1
 T 1
 O 1
 S 1
+ */
+function occurrences(str){
+    var counted = [];
+    if(typeof(str)=="string"){
+        for(var i=0;i<str.length;i++){
+            var a = str[i];
+            if(a==" "){
+                continue;
+            }
+            var count=0;
+            for(var j=0;j<str.length;j++){
+                if(a == str[j]){
+                    count+=1;
+                }
+            }
+            if(!(counted.includes(str[i]))){
+            console.log(str[i]+" "+count);
+            }
+            counted[i] = str[i];
+            // console.log(counted[i]);
+        }
+    }
+    else{
+        console.log("Please enter in correct(string) format");
+    }
+}
+occurrences("brain mentors");
+
+/**
+ OUTPUT
+b 1
+r 2
+a 1
+i 1
+n 2
+m 1
+e 1
+t 1
+o 1
+s 1
  */

65cb9f2a796f9c3203acfb7228f8c1cdd3b3f240
Abhishek Shukla <106401929+codelemma@users.noreply.github.com>
2023-06-30 00:06:22 +0530

    Update question9.js

--- question9.js
+++ question9.js
@@ -3,3 +3,20 @@
 Must contains @
 Must ends with .com/.org/.edu
  */
+function validateMail(x){
+    if(typeof(x) ==='string'){
+        if(x.match("([a-z0-9]+)\@gmail([.]{1}\\w{3})")){
+            console.log("Valid Mail");
+        }
+        else{
+            console.log("Please enter the correct Mail");
+        }
+    }
+    else{
+        console.log("Please enter the correct Format");
+    }
+}
+validateMail("oiuyt76r@gmail.oip");
+/* OUTPUT 
+Valid Mail
+ */

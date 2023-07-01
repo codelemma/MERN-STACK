5534cb972f7fc0161bf2bfc9fede377404731360
Abhishek Shukla <106401929+codelemma@users.noreply.github.com>
2023-07-01 16:37:26 +0530

    Update 1.js

--- assignments/Assignment-4/1.js
+++ assignments/Assignment-4/1.js
@@ -3,3 +3,17 @@
 arr= [19, 22, 18, 19, 16, 18, 19, 21, 24];
 target = 19;
 */
+/*
+1. Find Occurance of a target value in given array;
+arr= [19, 22, 18, 19, 16, 18, 19, 21, 24];
+target = 19;
+ */
+
+arr= [19, 22, 18, 19, 16, 18, 19, 21, 24];
+var occurances = arr.filter(e=>e==19).length;
+console.log(occurances);
+/*
+OUTPUT:
+3
+ */
+

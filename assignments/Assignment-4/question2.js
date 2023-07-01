3125a4f94c9dd172dd36210456ce539d14f24bd3
Abhishek Shukla <106401929+codelemma@users.noreply.github.com>
2023-07-01 16:38:41 +0530

    Create question2.js

---
+++ assignments/Assignment-4/question2.js
@@ -0,0 +1,14 @@
+/*
+2 . Reverse a given string using reverse() method 
+input = "BrainMentors"
+output = "srotnemniarB"
+ */
+var str = "BrainMentors";
+var strarr = str.split("");
+strarr = strarr.reverse();
+strarr = strarr.join("");
+console.log(strarr);
+/*
+output:
+srotneMniarB
+ */
